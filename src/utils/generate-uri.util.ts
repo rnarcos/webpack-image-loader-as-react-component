3ad53b657006a320c98809357175f89337b301e5
rnarcos <marcos@fernandescouto.com>
2021-03-25 16:25:51 -0300

    # Generate URI util: . Fix generateURI using wrong fileMimeType to generate uri string.

--- src/utils/generate-uri.util.ts
+++ src/utils/generate-uri.util.ts
@@ -1,7 +1,7 @@
 import mime from 'mime';
 
 export default (fileBuffer: Buffer, filePath: string): string => {
-  const fileMimeType = mime.getExtension(filePath) ?? '';
+  const fileMimeType = mime.getType(filePath) ?? '';
   const fileBase64Uri = fileBuffer.toString('base64');
 
   return `data:${fileMimeType};base64,${fileBase64Uri}`;
